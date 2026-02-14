b6b7052eaebefb4130daa50e4c17537f97dded0b
Ahmadayed22 <ahmad.ayed461@gmail.com>
2026-02-14 19:46:12 +0300

    object

--- src/App.jsx
+++ src/App.jsx
@@ -101,7 +101,7 @@ const MenuCarouselAdvanced = () => {
                   <img
                     src={image}
                     alt={`Menu page ${index + 1}`}
-                    className="w-full h-full object-contain rounded-2xl bg-white"
+                    className="w-full h-full p-6 rounded-2xl bg-white"
                     loading={index === currentIndex ? 'eager' : 'lazy'}
                   />
                 </div>
